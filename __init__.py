2c16ee46cdfa72163a449174f25250665a5a6a3c
alkislardeniz <alkislardeniz@gmail.com>
2020-04-14 17:37:49 +0300

    Added different browser support

--- __init__.py
+++ __init__.py
@@ -4,9 +4,18 @@ from selenium import webdriver
 
 from freemiumSpotify.FreemiumSpotify import *
 
+def create_driver(type):
+    if type.lower() == "chrome":
+        return webdriver.Chrome()
+    elif type.lower() == "firefox":
+        return webdriver.Firefox()
+    elif type.lower == "opera":
+        return webdriver.Opera()
+    elif type.lower == "safari":
+        return webdriver.Safari()
 
-def main(spotify_playlist_url):
-    driver = webdriver.Firefox()
+def main(spotify_playlist_url, type):
+    driver = create_driver(type)
     fs = FreemiumSpotify(spotify_playlist_url, driver)
     fs.retrieve_playlist_from_spotify()
     fs.find_and_download_mp3()
@@ -14,7 +23,7 @@ def main(spotify_playlist_url):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
     else:
-        print("Please give a Spotify playlist url as an argument!")
+        print("Valid call format: python __init__ <PLAYLIST_URL> <BROWSER_TYPE>")
